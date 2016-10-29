d9bfe9777501236a9583f584b417a0b0330cca06
Lee Dohm <lee@lee-dohm.com>
2016-10-29 15:38:49 -0700

    Add movement information

---
+++ src/hero/movement.js
@@ -0,0 +1,16 @@
+const movement = {
+  running: {
+    base: 12,
+    multiplier: 1
+  },
+  swimming: {
+    base: 4,
+    multiplier: 0.5
+  }
+  leaping: {
+    base: 4,
+    multiplier: 0.5
+  }
+}
+
+export default movement
