3518c89d5b70f186a0846629d2f7fb5f2cb1c0f4
Lee Dohm <lee@lee-dohm.com>
2016-10-29 13:49:33 -0700

    Add HeroMath.round function for handling rounding the Hero way

---
+++ src/renderer/hero-math.js
@@ -0,0 +1,41 @@
+/** @babel */
+
+/**
+ * Custom math library that follows the conventions described in the Hero System 6th Edition rules.
+ */
+export default class HeroMath {
+  /**
+   * Public: Rounds the `number` according to the Hero System 6th Edition rules.
+   *
+   * It always rounds numbers with a fractional part starting with `0.4` down (toward zero) and a
+   * fractional part starting with `0.6` up (away from zero). Numbers with a fractional part
+   * starting with `0.5` are rounded to the benefit of the player. The `direction` parameter
+   * indicates which direction would be more advantageous to the player. Pass `'up'` for away from
+   * zero and `'down'` for towards zero.
+   *
+   * See: "Character Points and Rounding" - 6E1 12
+   */
+  static round (number, direction) {
+    let sign = 1
+
+    if (number < 0) {
+      sign = -1
+    }
+
+    let magnitude = Math.abs(number)
+    let integer = Math.floor(magnitude)
+    let fracDigit = this.getFirstDecimalDigit(magnitude)
+
+    if (fracDigit > 5) {
+      integer += 1
+    } else if (fracDigit === 5 && direction === 'up') {
+      integer += 1
+    }
+
+    return integer * sign
+  }
+
+  static getFirstDecimalDigit (number) {
+    return Math.floor(number * 10) % 10
+  }
+}
---
+++ test/renderer/hero-math.test.js
@@ -0,0 +1,23 @@
+import {expect} from 'chai'
+
+import HeroMath from '../../src/renderer/hero-math'
+
+describe('HeroMath', function () {
+  describe('round', function () {
+    it('rounds down on a fractional part of 0.4 or less', function () {
+      expect(HeroMath.round(1.4)).to.equal(1)
+    })
+
+    it('rounds up on a fractional part of 0.6 or more', function () {
+      expect(HeroMath.round(1.6)).to.equal(2)
+    })
+
+    it('rounds up on fractional part of 0.5 when direction is up', function () {
+      expect(HeroMath.round(99.5, 'up')).to.equal(100)
+    })
+
+    it('rounds down on a fractional part of 0.5 when direction is down', function () {
+      expect(HeroMath.round(99.5, 'down')).to.equal(99)
+    })
+  })
+})
