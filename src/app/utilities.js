9564b89e7cc8cf2f6571a780e61c1028d533324b
Lee Dohm <lee@lee-dohm.com>
2016-10-29 19:52:17 -0700

    :memo: Document Utilities.getDataFile

--- src/app/utilities.js
+++ src/app/utilities.js
@@ -2,7 +2,21 @@ import {app} from 'electron'
 import fs from 'fs'
 import path from 'path'
 
+/**
+ * General utilities for the app.
+ *
+ * Usable in both the main and renderer processes.
+ */
 export default class Utilities {
+  /**
+   * Gets the named data file.
+   *
+   * Returns the {Object} containing the information from the file.
+   */
+  static getDataFile (name) {
+    return JSON.parse(fs.readFileSync(path.join(this.getDataPath(), `${name}.json`)))
+  }
+
   static getAppPath () {
     let appPath = app.getAppPath()
 
@@ -15,9 +29,5 @@ export default class Utilities {
 
   static getDataPath () {
     return path.join(this.getAppPath(), 'data')
-  }
-
-  static getDataFile (name) {
-    return JSON.parse(fs.readFileSync(path.join(this.getDataPath(), `${name}.json`)))
   }
 }
