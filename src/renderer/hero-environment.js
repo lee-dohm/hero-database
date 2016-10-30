16d0e41aac803f267606912d57439fac118232a8
Lee Dohm <lee@lee-dohm.com>
2016-10-30 12:54:05 -0700

    :memo: Document HeroEnvironment

--- src/renderer/hero-environment.js
+++ src/renderer/hero-environment.js
@@ -3,12 +3,22 @@ import path from 'path'
 
 const {app} = require('electron').remote
 
+/**
+ * Global utility class for storing information about the environment in which the application is
+ * running.
+ */
 export default class HeroEnvironment {
+  /**
+   * Creates a standard environment.
+   */
   constructor () {
     this.appPath = app.getAppPath()
     this.documentPath = app.getPath('documents')
   }
 
+  /**
+   * Returns the {String} path in which the application is installed.
+   */
   getAppPath () {
     if (process.env.HERO_DATABASE_HOME) {
       return process.env.HERO_DATABASE_HOME
@@ -17,6 +27,9 @@ export default class HeroEnvironment {
     }
   }
 
+  /**
+   * Returns an {Object} containing data that is bundled with the application.
+   */
   getData (name) {
     const dataPath = this.getDataPath(name)
 
@@ -33,6 +46,9 @@ export default class HeroEnvironment {
     return dataPath
   }
 
+  /**
+   * Returns the {String} path to where the application will store its documents.
+   */
   getDocumentPath () {
     return this.documentPath
   }
