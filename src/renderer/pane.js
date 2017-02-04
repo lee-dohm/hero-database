f64b947fb5b290b5195d25076534861d3f3694f0
Lee Dohm <lee@lee-dohm.com>
2017-02-04 07:16:55 -0800

    :memo:

--- src/renderer/pane.js
+++ src/renderer/pane.js
@@ -1,11 +1,19 @@
 import {Emitter} from 'event-kit'
 
+/**
+ * Represents the editor pane area of the UI.
+ */
 export default class Pane {
   constructor (heroEnv = hero) {
     this.emitter = new Emitter()
     this.heroEnv = heroEnv
   }
 
+  /**
+   * Called with the current active editor and whenever the active editor is changed.
+   *
+   * * `editor` Editor that is now active.
+   */
   observeEditor (callback) {
     this.emitter.on('did-change-editor', callback)
 
@@ -14,6 +22,11 @@ export default class Pane {
     }
   }
 
+  /**
+   * Sets the active editor.
+   *
+   * * `editor` Editor to make active.
+   */
   setEditor (editor) {
     this.editor = editor
 
--- src/renderer/workspace.js
+++ src/renderer/workspace.js
@@ -4,7 +4,7 @@ import RecordEditor from './record-editor'
 import WorkspaceView from './workspace-view'
 
 /**
- * Represents the contents of the current workspace.
+ * Represents the workspace.
  */
 export default class Workspace {
   constructor (heroEnv) {
@@ -12,14 +12,25 @@ export default class Workspace {
     this.heroEnv = heroEnv
   }
 
+  /**
+   * Called when a record has been opened for editing.
+   *
+   * * `editor` Editor for the record.
+   */
   onDidOpen (callback) {
     this.emitter.on('did-open', callback)
   }
 
+  /**
+   * Called when the UI has been loaded.
+   */
   onDidLoadUI (callback) {
     this.emitter.on('did-load-ui', callback)
   }
 
+  /**
+   * Opens a record for editing.
+   */
   async open (record) {
     const editor = new RecordEditor(record)
     this.heroEnv.pane.setEditor(editor)
